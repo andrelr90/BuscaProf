521e6f7c0e9c4023cde7914e7a3c00fd50fa9e90
pdrgbr <pdrgeovanni@gmail.com>
2021-01-25 23:47:25 -0300

    Adicionado o DB

---
+++ backend/db_project/dbConfig.js
@@ -0,0 +1,75 @@
+class Database{
+	Constructor(){}
+
+	async createDatabase(){
+		const mysqlCreate = require("mysql2");
+		const conn = mysqlCreate.createConnection({
+			host:'localhost',
+			user: 'root',
+			password: 'trebla321'
+		});
+		conn.query("CREATE DATABASE EngSoftDB");
+		conn.end();
+	}
+	
+	async function createTables(){
+		const conn = await connect();
+		const createUsers = "CREATE TABLE Users (id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(100) NOT NULL, name VARCHAR(20) NOT NULL, password VARCHAR(30) NOT NULL, professor BOOLEAN NOT NULL DEFAULT FALSE)";
+		const createSubjects = "CREATE TABLE Subjects (code VARCHAR(6) NOT NULL PRIMARY KEY, subjectName VARCHAR(60) NOT NULL)";
+		const createProfData = "CREATE TABLE ProfData (id INT PRIMARY KEY, description VARCHAR(2000), price INT, FOREIGN KEY (id) REFERENCES Users (id) ON DELETE CASCADE)";
+		const createMessages = "CREATE TABLE Messages (idSender INT, idReceiver INT, time DATETIME, status BOOLEAN NOT NULL DEFAULT FALSE, text VARCHAR(2000), PRIMARY KEY (idSender, idReceiver, time), FOREIGN KEY (idSender) REFERENCES Users (id) ON DELETE CASCADE, FOREIGN KEY (idReceiver) REFERENCES Users (id) ON DELETE CASCADE)";
+		const createSubProf = "CREATE TABLE SubProf (professor INT, subject VARCHAR(6), FOREIGN KEY (professor) REFERENCES Users (id) ON DELETE CASCADE, FOREIGN KEY (subject) REFERENCES Subjects (code) ON DELETE CASCADE	)";
+
+		conn.query(createUsers);
+		conn.query(createSubjects);
+		conn.query(createSubProf);
+		conn.query(createProfData);
+		conn.query(createMessages);
+
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC203', 'Programação e Desenvolvimento de Software I')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC638', 'Introdução à Lógica Computacional')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('MAT038', 'Geometria Analitica e Algebra Linear')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC204', 'Programação e Desenvolvimento de Software II')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC216', 'Matemática Discreta')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC639', 'Álgebra Linear Computacional')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('MAT001', 'Calculo Diferencial e Integral I')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC011', 'Introdução a Banco de Dados')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC114', 'Introdução a Sistemas Logicos')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC205', 'Estruturas de Dados')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('EST032', 'Probabilidade')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('MAT039', 'Calculo Diferencial e Integral II')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC006', 'Organização de Computadores I')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC129', 'Fundamentos da Teoria da Computação')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC206', 'Algoritmos I')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC212', 'Introdução à Ciência dos Dados')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('MAT034', 'Algebra A')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC299', 'Linguagens de Programação')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC035', 'Pesquisa Operacional')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC207', 'Algoritmos II')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('MAT040', 'Equações Diferenciais C')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC053', 'Compiladores I')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC603', 'Engenharia de Software')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC605', 'Sistemas Operacionais')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('FIS156', 'Introdução à Fisica Estatistica Computacional')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC023', 'Redes de Computadores')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC636', 'Etica na Computação')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC641', 'Fundamentos de Sistemas Paralelos e Distribuidos')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC642', 'Introdução à Inteligência Artificial')");
+		conn.query("INSERT INTO Subjects (code, subjectName) VALUES ('DCC637', 'Computação e Sociedade')");
+
+		conn.end();
+}
+
+	async connect(){
+		const mysql = require("mysql2/promise");
+		const connection = await mysql.createConnection("mysql://root:trebla321@localhost:3306/EngSoftDB");
+		return connection;
+	}
+
+	async disconcect(conn){
+		conn.end();
+	}
+
+}
+
+module.exports = Database;
--- backend/src/schema/UserSchema.js
+++ backend/src/schema/UserSchema.js
@@ -1,66 +1,96 @@
 const User = require('../model/User')
+const Database = require ('../../db_project/dbConfig');
+const db = new Database();
 
 class UserSchema {
     constructor() {
 
     }
     async createUser(user) {
+        const conn = await db.connect();
+        const sql = "INSERT INTO Users (email, name, password, professor) VALUES (?,?,?,?)";
+        const values = [user.email, user.name, user.password, user.professor];
+        const {success, err}  = conn.query(sql, values);
         //const query = `INSERT INTO Users (email, name, password, professor) VALUES (${user.email}, ${user.name}, ${user.password}, ${user.professor});´;
         //const {success, err} = db.runQuery(query);
-        const success = true;
-        const err = null; 
+        //const success = true;
+        //const err = null; 
         console.log(`CreateUser ${user}`)
+        db.disconnect(conn);
         return {success: success, err: err};
     }
 
     async updateUser(user) {
+        const conn = await db.connect();
+        const sql = "UPDATE Users SET email = ?, name = ?, password = ?, professor = ? WHERE id = ?";
+        const values = [user.email, user.name, user. password, user.professor, user.id];
+
+        const {success, err} = conn.query(sql, values);
         //const query = `UPDATE Users
         //SET email = '${user.email}', name = '${user.name}', password = '${user.password}', professor = '${user.professor}',
         //WHERE id = ${user.id};`
         
         //const {success, err} = db.runQuery(query);
-        const success = true;
-        const err = null;
+        //const success = true;
+        //const err = null;
+        db.disconnect(conn);
         return {success: success, err: err};
     }
 
     async deleteUser(user) {
+        const conn = await connect();
+        const sql = "DELETE FROM Users WHERE id = ?";
+        const {success, err} = conn.query(sql, [user.id]);
         //const query = `DELETE FROM Users WHERE id = ${user.id};`
 
         //const {success, err} = db.runQuery(query);
-        const success = true;
-        const err = null; 
+        //const success = true;
+        //const err = null; 
+        db.disconnect(conn);
         return {success: success, err: err};
     }
 
 
     async getUserByEmail(userToBeSearched) {
+        const conn = await db.connect();
+        const sql = "SELECT * FROM Users WHERE email = ?";
+        const {user, userFound, err} = conn.query(sql, [userToBeSearched.email]);
         //const query = `SELECT * FROM Users WHERE email = '${user.email}';`
 
         //const {user, userFound, err} = db.runSelectQuery(query);
-        const user = new User({email: userToBeSearched.email, id: 10});
-        const userFound = true;
-        const err = null; 
+        //const user = new User({email: userToBeSearched.email, id: 10});
+        //const userFound = true;
+        //const err = null;
+        db.disconnect(conn); 
         return {user: user, userFound: userFound, err: err};
     }
 
     async getUserById(userToBeSearched) {
+        const conn = await db.connect();
+        const sql = "SELECT * FROM Users WHERE id = ?";
+        const {user, userFound, err} = conn.query(sql, [userToBeSearched.id]);
         //const query = `SELECT * FROM Users WHERE id = ${user.id};`
 
         //const {user, userFound, err} = db.runSelectQuery(query);
-        const user = new User({id: userToBeSearched.id, email: "teste"});
-        const userFound = true;
-        const err = null;
+        //const user = new User({id: userToBeSearched.id, email: "teste"});
+        //const userFound = true;
+        //const err = null;
+        db.disconnect(conn);
         return {user: user, userFound: userFound, err: err};
     }
 
     async getUserByName(userToBeSearched) {
+        const conn = await db.connect();
+        const sql = "SELECT * FROM Users WHERE name = ?";
+        const {user, userFound, err} = conn.query(sql, [userToBeSearched.name]);
+
         //const query = `SELECT * FROM Users WHERE name = ${user.name};`
 
         //const {user, userFound, err} = db.runSelectQuery(query);
-        const user = new User({name: userToBeSearched.name, email: "teste"});
-        const userFound = true;
-        const err = null;
+        //const user = new User({name: userToBeSearched.name, email: "teste"});
+        //const userFound = true;
+        //const err = null;
+        db.disconnect(conn);
         return {user: user, userFound: userFound, err: err};
     }
 
