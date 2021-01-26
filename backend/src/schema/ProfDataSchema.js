aef40921869501f4501ab935efcdfedda434d0de
IgorCastejon <igorcastejon@dcc.ufmg.br>
2021-01-26 13:48:50 -0300

    DB query standard

--- backend/src/schema/ProfDataSchema.js
+++ backend/src/schema/ProfDataSchema.js
@@ -9,15 +9,16 @@ class ProfDataSchema {
     async createProfData(prof) {
         const conn = await db.connect();
         const sql = "INSERT INTO ProfData (id, description, price) VALUES (?,?,?)";
-        const values = [prof.id, prof.description, prof.price];
-        const {success, err} = conn.query(sql, values);
-
-        //const query = `INSERT INTO ProfData (id, description, price) VALUES (${prof.id}, \"\", 0);´;
-        //const {success, err} = db.runQuery(query);
-        // const success = true;
-        // const err = null; 
-        
-        console.log(`CreateProfData ${prof}`);
+        const values = [prof.id, prof.description, prof.price]
+        let success = null;
+        let err = null;
+        try {
+            success = await conn.query(sql, values);
+        }
+        catch(error) {
+            err = error;
+        }
+        console.log(`CreateProfData ${prof}`)
         db.disconnect(conn);
         return {success: success, err: err};
     }
@@ -56,15 +57,21 @@ class ProfDataSchema {
     async getProfDataById(prof) {
         const conn = await db.connect();
         const sql = "SELECT * FROM ProfData WHERE id = ?";
-        const {[[user]], userFound, err} = await conn.query(sql, [prof.id]);
+        let err = null;
+        let userFound = false;
+        let users = null;
+        try {
+            const [rows, _] = await conn.execute(sql, [prof.id]);
+            if (rows.length > 0) {
+                users = rows[0];
+                userFound = true;
+            }
+        }
+        catch(error) {
+            err = error;
+        }
 
-        //const query = `SELECT * FROM ProfData WHERE id = ${prof.id};
-        //const {user, userFound, err} = db.runSelectQuery(query);
-        // const user = new ProfData({id: 10});
-        // const userFound = true;
-        // const err = null;
-
-        db.disconnect(conn); 
+        db.disconnect(conn);
         return {user: user, userFound: userFound, err: err};
     }
 
