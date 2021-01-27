e1b2fae20eba1d838fbc23ffea1791cda0e5b6c5
IgorCastejon <igorcastejon@dcc.ufmg.br>
2021-01-26 23:18:32 -0300

    Remove contact duplicates

--- backend/src/schema/MessageSchema.js
+++ backend/src/schema/MessageSchema.js
@@ -79,8 +79,8 @@ class MessageSchema {
 
     async getContacts(message) {
         const conn = await db.connect();
-        const sql = "SELECT M.idSender, M.idReceiver, S.name as nameSender, R.name as nameReceiver FROM Messages M JOIN Users S ON M.idSender = S.id OR M.idReceiver = S.id JOIN Users R ON M.idSender = R.id OR M.idReceiver = R.id WHERE (M.idSender = ? AND M.idReceiver = ?) OR (M.idSender = ? AND M.idReceiver = ?)";
-        const values = [message.idSender, message.idReceiver, message.idReceiver, message.idSender];
+        const sql = "SELECT M.idSender, M.idReceiver, S.name as nameSender, R.name as nameReceiver FROM Messages M JOIN Users S ON M.idSender = S.id OR M.idReceiver = S.id JOIN Users R ON M.idSender = R.id OR M.idReceiver = R.id WHERE (M.idSender = ? OR M.idReceiver = ?)";
+        const values = [message.idSender, message.idSender, message.idReceiver, message.idSender];
         
         let err = null;
         let contactFound = false;
@@ -94,13 +94,13 @@ class MessageSchema {
                 for (let row of rows) {
                     let idSender = row.idSender;
                     let idReceiver = row.idReceiver;
-                    let nameSender = nameSender;
-                    let nameReceiver = nameReceiver;
+                    let nameSender = row.nameSender;
+                    let nameReceiver = row.nameReceiver;
                     if (idSender != message.idSender) {
-                        set.add({id idSender, name: nameSender});
+                        set.add(JSON.stringify({id: idSender, name: nameSender}));
                     }
                     if (idReceiver != message.idSender) {
-                        set.add(id: idReceiver, name: nameReceiver);
+                        set.add(JSON.stringify({id: idReceiver, name: nameReceiver}));
 
                     }
                 }
@@ -112,7 +112,11 @@ class MessageSchema {
             err = error;
         }
 
-        let uniq = [...set];
+        let uniqJson = [...set];
+        let uniq = []
+        for (let json of uniqJson) {
+            uniq.push(JSON.parse(json));
+        }
         
         db.disconnect(conn); 
         return {idContacts: uniq, contactFound: contactFound, err: err};
