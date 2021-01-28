9883e052e89fddf1b4e84e2fe9302fddf8d4c54d
IgorCastejon <igorcastejon@dcc.ufmg.br>
2021-01-28 20:32:45 -0300

    Contact notifications

--- backend/src/schema/MessageSchema.js
+++ backend/src/schema/MessageSchema.js
@@ -27,7 +27,7 @@ class MessageSchema {
     async getMessages(message) {
         const conn = await db.connect();
         const sql = "SELECT M.idSender AS id, M.text AS body, U.name AS author, DATE_FORMAT(M.time, '%d/%m/%Y %h:%i:%s') as time FROM Messages M JOIN Users U ON M.idSender = U.id WHERE (M.idSender = ? AND M.idReceiver = ?) OR (M.idSender = ? AND M.idReceiver = ?) ORDER BY M.time ASC";
-        const sqlSetNotifications = "UPDATE Messages SET status = 1 WHERE idSender = ? OR idReceiver = ? OR idSender = ? OR idReceiver = ?";
+        const sqlSetNotifications = "UPDATE Messages SET status = 1 WHERE idReceiver = ?";
         const values = [message.idSender, message.idReceiver, message.idReceiver, message.idSender];
         
         let err = null;
@@ -35,7 +35,7 @@ class MessageSchema {
         let messages = null;
         try {
             const [rows, _] = await conn.execute(sql, values);
-            await conn.execute(sqlSetNotifications, values);
+            await conn.execute(sqlSetNotifications, [message.idSender]);
             if (rows.length > 0) {
                 messages = rows;
                 messageFound = true;
@@ -84,29 +84,29 @@ class MessageSchema {
 
     async getContacts(message) {
         const conn = await db.connect();
-        const sql = "SELECT M.idSender, M.idReceiver, S.name as Sender, R.name as Receiver FROM ((Messages M JOIN Users S ON M.idSender = S.id) JOIN Users R ON M.idReceiver = R.id) WHERE (M.idSender = ? OR M.idReceiver = ?);";
+        const sql = "SELECT M.idSender, M.idReceiver, M.status, S.name as Sender, R.name as Receiver FROM ((Messages M JOIN Users S ON M.idSender = S.id) JOIN Users R ON M.idReceiver = R.id) WHERE (M.idSender = ? OR M.idReceiver = ?);";
         const values = [message.idSender, message.idSender];
         
         let err = null;
         let contactFound = false;
         let set = new Set();
-
         try {
             const [rows, _] = await conn.execute(sql, values);
             console.log(rows)
             console.log(rows.length)
+            
             if (rows.length > 0) {
                 for (let row of rows) {
                     let idSender = row.idSender;
                     let idReceiver = row.idReceiver;
                     let nameSender = row.Sender;
                     let nameReceiver = row.Receiver;
+                    
                     if (idSender != message.idSender) {
-                        set.add(JSON.stringify({id: idSender, name: nameSender}));
+                        set.add(JSON.stringify({id: idSender, name: nameSender, notifications: false}));
                     }
                     if (idReceiver != message.idSender) {
-                        set.add(JSON.stringify({id: idReceiver, name: nameReceiver}));
-
+                        set.add(JSON.stringify({id: idReceiver, name: nameReceiver, notifications: false}));
                     }
                 }
                 idContacts = rows;
@@ -116,15 +116,40 @@ class MessageSchema {
         catch(error) {
             err = error;
         }
+        const notifications = await this.getNotifications(new Message({idReceiver: message.idSender}));
+        let notificationsId = {}
+        for (let pending of notifications.idContacts) {
+            notificationsId[pending] = true;
+        }
 
         let uniqJson = [...set];
-        let uniq = []
+        let uniqNoNotifications = []
+        let uniqNotifications = []
         for (let json of uniqJson) {
-            uniq.push(JSON.parse(json));
+            uniqNoNotifications.push(JSON.parse(json));
+        }
+        console.log(uniqNoNotifications)
+        for (let user of uniqNoNotifications) {
+            let userNotifiedOrNot = user;
+            let id = 0;
+            try {
+                id = userNotifiedOrNot.id;
+            }
+            catch(err) {
+                console.log(err)
+            }
+            if (id in notificationsId) {
+                userNotifiedOrNot.notifications = true;
+            }
+            else {
+                userNotifiedOrNot.notifications = false;
+            }
+            
+            uniqNotifications.push(userNotifiedOrNot);
         }
         
         db.disconnect(conn); 
-        return {idContacts: uniq, contactFound: contactFound, err: err};
+        return {idContacts: uniqNotifications, contactFound: contactFound, err: err};
     }
 
 }
